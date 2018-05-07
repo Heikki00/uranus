99d98c6f6bbdd347ed8d7f1a7d3f1da3106b2fc0
Heikki Simojoki <heikki.simojoki@gmail.com>
2018-05-07 20:08:11 +0300

    I should get a life

--- public/js/main.js
+++ public/js/main.js
@@ -24,9 +24,11 @@
 		}, 600);
 	});
 
-	$('#back-to-top').on('click', function(){
-		$('body,html').animate({
-			scrollTop: 0
+	$(" .home-content a[href^='#']").on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
 		}, 600);
 	});
 
